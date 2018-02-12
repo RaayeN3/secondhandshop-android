7747b1e32e48f05a94d2458c1c86e2a6b2f7761d
cecobask <32335835+cecobask@users.noreply.github.com>
2018-02-12 18:46:20 +0000

    Getting started. Main activity in process of development.

---
+++ app/src/main/java/ie/bask/niftysecondhandshop/MainActivity.java
@@ -0,0 +1,60 @@
+package ie.bask.niftysecondhandshop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button sellButton;
+    private ImageButton homeButton;
+    private ImageButton cameraButton;
+    private ImageButton settingsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        sellButton = (Button) findViewById(R.id.sellButton);
+        if (sellButton != null)
+        {
+            Log.v("ShopMainActivity", "Sell button initialised!");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sellButtonPressed (View view)
+    {
+        Log.v("ShopMainActivity", "Sell button pressed!");
+    }
+}
